39eba2c161e7eac87227c01ad11343a164dd5675
yujin <84204420+euphorxa@users.noreply.github.com>
2022-11-26 23:12:08 -0800

    Alright nyxx it is

---
+++ bin/running_on_dart.dart
@@ -0,0 +1,25 @@
+import 'package:dotenv/dotenv.dart';
+import 'package:nyxx/nyxx.dart';
+import 'package:nyxx_commands/nyxx_commands.dart';
+
+void main() async {
+  final env = DotEnv(includePlatformEnvironment: true)..load();
+  final token = env["TOKEN"];
+  final bot = NyxxFactory.createNyxxWebsocket(
+      token!,
+      GatewayIntents.guildBans |
+      GatewayIntents.directMessages
+  );
+
+  bot
+    ..registerPlugin(Logging())
+    ..registerPlugin(CliIntegration());
+
+  CommandsPlugin commands = CommandsPlugin(
+    prefix: (message) => "<3"
+  );
+
+  bot.registerPlugin(commands);
+
+  await bot.connect();
+}
